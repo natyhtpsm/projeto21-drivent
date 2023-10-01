172481d89bd4be36c675a518f083af1db103d37b
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-01 20:57:07 -0300

    Feat: payment error, hotels service

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -1,3 +1,31 @@
 import { hotelsRepository } from "@/repositories/hotels-repository";
 import { ticketsRepository } from "@/repositories";
 import { enrollmentRepository } from "@/repositories";
+import { notFoundError } from "@/errors";
+import { paymentRequiredError } from "@/errors/payment-requred-error";
+
+async function verifyEnrollmentTicket(userId: number){
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    if(!enrollment){
+        throw notFoundError();
+    }
+
+    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+    if(!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel){
+        throw paymentRequiredError();
+    }
+}
+
+async function getHotels(userId: number) {
+    return await hotelsRepository.getHotels();
+}
+
+async function getHotelById(id: number, userId: number) {
+    await verifyEnrollmentTicket(userId);
+    return await hotelsRepository.getHotelById(id);
+}
+
+export const hotelsService = {
+    getHotelById,
+    getHotels,
+};
