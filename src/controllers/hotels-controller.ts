e130a49360f82d2dcb4257a7b7953a084c852dce
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-02 10:31:24 -0300

    Fix: errors being handled twice

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -5,37 +5,13 @@ import { hotelsService } from "@/services/hotels-service";
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
     const { userId } = req;
-    try{
-        const hotels = await hotelsService.getHotels(userId);
-        if(!hotels){
-            return res.status(httpStatus.NOT_FOUND).json(hotels);
-        }
-        return res.status(httpStatus.OK).json(hotels);
-    } catch(error){
-        if(error.name === "notFoundError"){
-            return res.status(httpStatus.NOT_FOUND).json(error);
-        }
-        if(error.name === "paymentRequiredError"){
-            return res.status(httpStatus.PAYMENT_REQUIRED).json(error);
-        }
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
-    }
+    const hotels = await hotelsService.getHotels(userId)
+    return res.status(httpStatus.OK).json(hotels);
 }
 
 export async function getHotelById(req: AuthenticatedRequest, res: Response) {
     const { userId } = req;
     const { id } = req.params;
-    try{
-        const hotel = await hotelsService.getHotelById(Number(id), userId);
-        res.status(httpStatus.OK).json(hotel);
-    } catch(error){
-        if(error.name === "notFoundError"){
-            return res.status(httpStatus.NOT_FOUND).json(error);
-        }
-        if(error.name === "paymentRequiredError"){
-            return res.status(httpStatus.PAYMENT_REQUIRED).json(error);
-        } else{
-            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
-        }
-    }
+    const hotel = await hotelsService.getHotelById(Number(id), userId);
+    res.status(httpStatus.OK).json(hotel);
 }
