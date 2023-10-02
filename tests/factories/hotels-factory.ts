8af7401c9a20b5388f7be07cbc8b9e2dacda1ee0
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-01 22:10:13 -0300

    Feat: faker hotels

---
+++ tests/factories/hotels-factory.ts
@@ -0,0 +1,21 @@
+import faker from "@faker-js/faker";
+import { prisma } from "@/config";
+
+export async function createHotel(params: any = {}) {
+    return prisma.hotel.create({
+        data: {
+            name: params.name || faker.lorem.words(2),
+            image: params.image || faker.image.imageUrl(),
+        },
+    });
+}
+
+export async function createHotelWithRooms(id: number) {
+    return prisma.room.create({
+        data: {
+            name: faker.lorem.words(2),
+            capacity: faker.datatype.number(10),
+            hotelId: id,
+        },
+    });
+}
