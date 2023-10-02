dc0531fa651cb0c122d8f572bc056e873fde65e7
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-01 21:04:36 -0300

    Feat: hotels controller

---
+++ src/controllers/hotels-controller.ts
@@ -0,0 +1,34 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+import httpStatus from "http-status";
+import { hotelsService } from "@/services/hotels-service";
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    try{
+        const hotels = await hotelsService.getHotels(userId);
+        res.status(httpStatus.OK).json(hotels);
+    } catch(error){
+        if(error.name === "notFoundError"){
+            return res.status(httpStatus.NOT_FOUND).json(error);
+        }
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+    }
+}
+
+export async function getHotelById(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const { id } = req.params;
+    try{
+        const hotel = await hotelsService.getHotelById(Number(id), userId);
+        res.status(httpStatus.OK).json(hotel);
+    } catch(error){
+        if(error.name === "notFoundError"){
+            return res.status(httpStatus.NOT_FOUND).json(error);
+        }
+        if(error.name === "paymentRequiredError"){
+            return res.status(httpStatus.PAYMENT_REQUIRED).json(error);
+        }
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error);
+    }
+}
