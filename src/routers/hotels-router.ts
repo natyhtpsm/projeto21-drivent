8997a6c5735257f859303e340fc69fc7286133cb
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-01 21:07:24 -0300

    Update: hotels routers

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { AuthenticatedRequest } from "@/middlewares";
 import { authenticateToken } from "@/middlewares";
+import { getHotelById, getHotels } from "@/controllers/hotels-controller";
 
 const hotelRouters = Router();
 
 hotelRouters
-    .all("/*", authenticateToken);
 
+    .all("/*", authenticateToken)
+    .get("/", getHotels)
+    .get("/:id", getHotelById);
 
 export { hotelRouters }
 
