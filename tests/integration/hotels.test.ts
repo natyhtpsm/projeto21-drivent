5f5ff37af3f102db2019359bcd7d33cbcb7ba763
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-01 23:05:38 -0300

    Feat: tests hotels

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -1,16 +1,47 @@
 import app, { init } from "@/app";
+import faker from "@faker-js/faker";
+import httpStatus from "http-status";
+import * as jwt from "jsonwebtoken";
 import supertest from "supertest";
 import { createUser } from "../factories";
 import { createHotel, createHotelWithRooms } from "../factories/hotels-factory";
 import { cleanDb, generateValidToken } from "../helpers";
 
-
-
 beforeAll(async () => {
   await init();
-    const server = supertest(app);
 });
 
 beforeEach(async () => {
   await cleanDb();
+});
+const server = supertest(app);
+
+describe("GET /hotels", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    const response = await server.get("/hotels");
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const invalidToken = faker.lorem.word();
+    const response = await server.get("/hotels").set("Authorization", `Bearer ${invalidToken}`);
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const tokenWithoutSession = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+    const response = await server.get("/hotels").set("Authorization", `Bearer ${tokenWithoutSession}`);
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 402 when user ticket is remote ", async () => {
+        const user = await createUser();
+        const token = generateValidToken(user);
+        const response = await server.get("/hotels").set("Authorization", `Bearer ${token}`);
+        expect(response.status).toBe(httpStatus.PAYMENT_REQUIRED);
+    });
+
+  });
 });
